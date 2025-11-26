8ef3e2a973e50cb580b71cbeb8c95e064e1523d7
Dev-sungmin <sungminmail0@naver.com>
2025-11-26 16:04:58 +0900

    jenkins nth try

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-cd /var/lib/jenkins/backend  # docker-compose.yml이 있는 디렉토리로 이동
-
-# 기존 컨테이너 정리
-docker compose down --remove-orphans
-
-# 사용하지 않는 이미지 정리 (선택사항)
-docker image prune -f
-
-# 최신 이미지로 재배포
+cd /home/ubuntu/backend
+docker compose down
 docker compose pull
 docker compose up -d
-
-# 컨테이너 상태 확인
-docker compose ps
