352fc1bbc43fa6641f58ef82c349d51295bf4113
Dev-sungmin <sungminmail0@naver.com>
2025-11-26 15:49:49 +0900

    jenkins 3rd try

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
-cd /var/lib/jenkins/backend
+#!/bin/bash
+
+cd /var/lib/jenkins/backend  # docker-compose.yml이 있는 디렉토리로 이동
+
+# 기존 컨테이너 정리
+docker compose down --remove-orphans
+
+# 사용하지 않는 이미지 정리 (선택사항)
+docker image prune -f
+
+# 최신 이미지로 재배포
 docker compose pull
 docker compose up -d
+
+# 컨테이너 상태 확인
+docker compose ps
